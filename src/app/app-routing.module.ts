20fb85817915a8953a2debe7382ccf1f9bbe33b0
Fekos2 <jp.ponceanima@gmail.com>
2022-05-07 22:38:53 -0400

    ESTETICA

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { TuHuellaComponent } from './tu-huella/tu-huella.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { CompensarComponent } from './compensar/compensar.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'tu-huella',
+    component: TuHuellaComponent
+  },
+  {
+    path: 'galeria',
+    component: GaleriaComponent
+  },
+  {
+    path: 'compensar',
+    component: CompensarComponent
+  },
+  {
+    path: 'registrarse',
+    component: RegistrarseComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
